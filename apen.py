a19ab53dc834cce48fe370e77019f6f5f57e040a
Ovidiu Valea <ovalea@bmajor.ro>
2020-04-17 00:40:22 +0300

    Added functionality to search a box by name and get IP

--- apen.py
+++ apen.py
@@ -1,29 +1,47 @@
-import os, json, time
+import os, json, argparse
 import libtmux, htb
 from box import Box
+
+parser = argparse.ArgumentParser(description='What box do you want to pwn?')
+parser.add_argument('--box', required=True)
+name = parser.parse_args()
 api = htb.HTB('OwBnueBa1zprFdqbWRQnNiXpyr0T1lIkZFQrwGUK0xnjD4Rs3yQxuUEGlHec')
 listOfBoxes = []
 #print(a.get_machine(7))
 #print(type(a.get_machine(7)))
 #list = a.get_machine(7).get("ip")
 #print(list)
-def print_all_boxes():
+
+def initBoxes():
     rawBoxes = api.get_machines()
-    #print(all)
-    #print(*all, sep='\n')
     for rawBox in rawBoxes:
         listOfBoxes.append(Box(rawBox))
+
+def printAllBoxes():
+    for box in listOfBoxes:
+        print(box.__repr__())
+    #print(all)
+    #print(*all, sep='\n')
+
         #print(el)
         #print(el.get("name"))
         #print(el["ip"])
-    for box in listOfBoxes:
-        print(box.__repr__())
+
     #formatlist = str(a.get_machines()).strip('[]')
     #print(type(formatlist))
     #formatlist = formatlist.replace("'", "\"")
     #res = json.loads(formatlist)
     #print(res)
     #list = res.get("name")
+
+def getBoxIP(name: str):
+    for box in listOfBoxes:
+        #for attribute, value in box.__dict__.items():
+        if box.__dict__.get("name") == name.box:
+            print(box.__dict__.get("ip"))
+        #if box.get("name") == name:
+        #    print(box["ip"])
+
 
 def workflow():
     os.system("xfce4-terminal -e 'tmux new-session -s sisc'")
@@ -32,8 +50,10 @@ def workflow():
     session = server.get_by_id('$0')
     window = session.attached_window
     window.rename_window('vpn')
-    
+
     session.new_window(attach=False, window_name="nmap")
 
-#print_all_boxes()
-workflow()
+initBoxes()
+print_all_boxes()
+#workflow()
+getBoxIP(name)
