48e1b295f25e7cf48decc4ed202316eedf73e3cb
Ovidiu Valea <ovalea@bmajor.ro>
2020-04-26 22:13:43 +0300

    Nmap search to CSV, get CVEs and find the exploit paths.

--- apen.py
+++ apen.py
@@ -1,6 +1,10 @@
-import os, json, argparse, requests
-import libtmux, htb
+import os, json, argparse, requests, time, csv, re
+import libtmux, htb, nmap
 from box import Box
+import pandas as pd
+import cve_searchsploit as CS
+import getsploit
+import pyperclip
 
 parser = argparse.ArgumentParser(description='What box do you want to pwn?')
 parser.add_argument('--box', required=True)
@@ -74,16 +78,92 @@ def controlBox(name: str, action: str):
         print("Error:" + (r["status"]))
 
 def workflow():
-    os.system("xfce4-terminal -e 'tmux new-session -s sisc'")
-    #time.sleep(1)
-    server = libtmux.Server()
-    session = server.get_by_id('$0')
-    window = session.attached_window
-    window.rename_window('vpn')
-    session.new_window(attach=False, window_name="nmap")
+    CS.update_db()
+    #Open a terminal with tmux
+    # os.system("xfce4-terminal -e \'bash -c \"tmux new-session -s sisc; bash\"\'")
+    # time.sleep(1)
+    # server = libtmux.Server()
+    # session = server.find_where({ "session_name": "sisc" })
+    # window = session.attached_window
+    # window.rename_window('nmap')
+    # paneVpn = window.attached_pane
+    # time.sleep(10)
+    # paneVpn.send_keys("nmap --script vuln -sC -sV -Pn -oA " + name.box +" " + getBoxIP(name))
+    cveList = []
+    with open(name.box + ".nmap", newline='') as nmapfile:
+        cves=[]
+        lines = nmapfile.read().splitlines()
+        for line in lines:
+            cves = re.findall(r'CVE-\w+-\w+', line)
+            for cve in cves:
+                cveList.append(cve)
+            # if 'CVE' in line:
+            #     print(line)
+    #paneVpn.send_keys("")
+    cveList = list(dict.fromkeys(cveList))
+    exploitList = []
+    for cve in cveList:
+        print(cve)
+        print(CS.edbid_from_cve(cve))
+        for i in CS.edbid_from_cve(cve):
+            os.system("searchsploit -p " + str(i))
+            #print(pyperclip.paste())
+            exploitList.append(pyperclip.paste())
+            time.sleep(5)
+    print(exploitList)
+    for exploit in exploitList:
+        print(exploit)
+    # with open("exploits.txt", newline='') as exploitfile:
+    #     exploits=[]
+    #     lines = exploitfile.read().splitlines()
+    #     for line in lines:
+    #         print(line)
+    #         exploits = re.findall(r'exploits/\w+/w+/$\w+', line)
+    #         for exploit in exploits:
+    #             print(exploit)
+    nm = nmap.PortScanner()
+    #nm.scan(hosts=getBoxIP(name), arguments='-sC -sV -Pn')
+    #nm.scan(hosts=getBoxIP(name), arguments='--script nmap-vulners,vulscan --script-args vulscandb=scipvuldb.csv -sC -sV -Pn')
+    #print(nm.csv())
+    #print(nm.command_line())
+    #with open('nmapOutput.csv', 'w') as f:
+#        print(nm.csv(), file=f)
+    # with open('nmapOutput.csv', 'w', newline='') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerows(nm.csv())
+    # productList = []
+    # with open('nmapOutput.csv', newline='') as csvfile:
+    #     spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    #     for row in spamreader:
+    #         print(', '.join(row))
+    #         if(row and row[7]):
+    #             print(row[7])
+    #             print("\n")
+    #             productList.append(row[7])
+    # for i in productList:
+    #     print(i)
+    #CS.edbid_from_cve("CVE-2019-0708")
+        # for line in csvfile:
+	    #        csvfile.write(line.replace(';', ','))
+        # df = pd.read_csv(csvfile)
+        # column = df['product']
+        # print(column)
+    # #Start HTB VPN on a window
+    # window.rename_window('vpn')
+    # paneVpn = window.split_window(attach=True)
+    # paneVpn.send_keys('openvpn DonDada.ovpn')
+    # time.sleep(5)
+    #paneVpn.send_keys('Menime1212!')
+    #time.sleep(10)
+    #Add a new window for nmap
+    # windowsNmap = session.new_window(attach=True, window_name="nmap")
+    # paneNmap = windowsNmap.split_window(attach=True)
+    # paneNmap.send_keys("nmap -sC -sV -oA " + name.box +" " + getBoxIP(name))
+    #Add a new window for searchsploit
+
 
 initBoxes()
 #Switch to assign/remove box
-controlBox(name, "remove")
+controlBox(name, "assign")
 #printAllBoxes()
-#workflow()
+workflow()
